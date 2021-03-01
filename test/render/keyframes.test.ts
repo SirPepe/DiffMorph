254ab17d63eef5988f2848b3a7e4c3eebe5277ae
Peter Kröner <peter@peterkroener.de>
2021-03-01 13:02:49 +0100

    test the bare minimum for keyframes

--- test/render/keyframes.test.ts
+++ test/render/keyframes.test.ts
@@ -4,10 +4,16 @@ import { toKeyframes } from "../../src/render/keyframes";
 import { type } from "../helpers";
 const json = type(language);
 
-describe.only("asdf", () => {
-  test("asdf", () => {
+describe("Keyframes", () => {
+  test("It turns some JSON into keyframes", () => {
     const diffs = diffAll([json("{}"), json("  {}"), json("    {}")]);
     const keyframes = toKeyframes(diffs);
-    console.log(keyframes);
+    expect(keyframes.length).toBe(3);
+    // All keyframe token ids should be equal
+    const aids = Array.from(keyframes[0].keys());
+    const bids = Array.from(keyframes[1].keys());
+    const cids = Array.from(keyframes[2].keys());
+    expect(aids).toEqual(bids);
+    expect(bids).toEqual(cids);
   });
 });
