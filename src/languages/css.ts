50a864d1371d6d36e7e156e3499ce36872bd9ee3
Peter Kröner <peter@peterkroener.de>
2021-03-04 16:29:35 +0100

    improve language definition for CSS

--- src/languages/css.ts
+++ src/languages/css.ts
@@ -1,23 +1,93 @@
+import { isAdjacent } from "../lib";
 import { LanguageToken, TypedLanguageToken } from "../types";
 
 const STRINGS = ["'", '"', "`"];
 
+const MEDIA_TYPES = new Set([
+  "all",
+  "braille",
+  "embossed",
+  "handheld",
+  "print",
+  "projection",
+  "screen",
+  "speech",
+  "tty",
+  "tv",
+]);
+
+const UNITS = [
+  "em",
+  "ex",
+  "%",
+  "px",
+  "cm",
+  "mm",
+  "in",
+  "pt",
+  "pc",
+  "ch",
+  "rem",
+  "vh",
+  "vw",
+  "vmin",
+  "vmax",
+  "deg",
+  "grad",
+  "rad",
+  "turn",
+  "s",
+  "ms",
+  "hz",
+  "khz",
+  "dpi",
+  "dpcm",
+  "dppx",
+];
+
+const NUMERIC_RE = new RegExp(`^(\\d+(\\.\\d+)?|\\.\\d+)(${ UNITS.join("|") })?$`);
+
 type State = {
   stringState: boolean | string; // string indicates the quote used
   commentState: boolean;
+  urlState: boolean;
+  atHeaderState: boolean;
+  ruleContext: "left" | "right" | "none";
   contextStack: string[];
 };
+
+function getContext(state: State) {
+  if (state.contextStack.length === 0) {
+    return "none"
+  }
+  return state.contextStack[state.contextStack.length - 1];
+}
+
+function parseNumeric (token: LanguageToken): string[] | null {
+  if (token.text === "." && token.next && isAdjacent(token, token.next)) {
+    const rest = parseNumeric(token.next);
+    if (rest) {
+      return ["number", ...rest];
+    }
+  } else if (NUMERIC_RE.test(token.text)) {
+    return ["number"]
+  }
+  return null;
+}
 
 const defaultState = (): State => ({
   stringState: false,
   commentState: false,
+  urlState: false,
+  atHeaderState: false,
+  ruleContext: "none",
   contextStack: [],
 });
 
-export const languageDefinition = (): ((token: LanguageToken) => string) => {
+export const languageDefinition = (): ((token: LanguageToken) => string | string[]) => {
   const state = defaultState();
 
-  return (token: LanguageToken): string => {
+  return (token: LanguageToken): string | string[] => {
     // exit comment state
     if (
       state.commentState === true &&
@@ -46,16 +116,143 @@ export const languageDefinition = (): ((token: LanguageToken) => string) => {
     // exit string state
     if (token.text === state.stringState) {
       state.stringState = false;
+      if (state.urlState) {
+        return "string-url";
+      }
       return "string";
     }
     // enter string state
     if (STRINGS.includes(token.text) && state.stringState === false) {
       state.stringState = token.text;
+      if (state.urlState) {
+        return "string-url";
+      }
       return "string";
     }
     // are we in string state?
     if (state.stringState) {
+      if (state.urlState) {
+        return "string-url";
+      }
       return "string";
+    }
+
+    // exit url state
+    if (state.urlState && token.text === ")") {
+      state.urlState = false;
+      return "punctuation-url";
+    }
+    // enter url state
+    if (token.text.toLowerCase() === "url" && token?.next?.text === "(") {
+      state.urlState = true;
+      return ["keyword-url", "punctuation-url"];
+    }
+    // are we in url state? If the url is quoted, string logic takes over
+    if (state.urlState) {
+      return "string-url";
+    }
+
+    // enter @rule
+    if (token.text === "@" && token?.next?.text.match(/^[a-z-]+$/)) {
+      const value = `at-${token?.next?.text}`;
+      // Don't treat @import and @charset as a context, as there can be nothing
+      // nested inside
+      if (value !== `at-import` && value !== `at-charset`) {
+        state.contextStack.push(value);
+        state.atHeaderState = true;
+      }
+      return [`keyword-${value}`, `keyword-${value}`];
+    }
+
+    // @rule header
+    if (getContext(state).startsWith("at-")) {
+      if (token.text === "}") {
+        const value = `punctuation-${getContext(state)}-end`;
+        state.contextStack.pop();
+        return value;
+      }
+      if (state.atHeaderState) {
+        if (getContext(state).endsWith("media") && MEDIA_TYPES.has(token.text)) {
+          return "keyword-media-type";
+        }
+        if (token.text === "(" || token.text === ")") {
+          return `punctuation-${getContext(state)}-argument`;
+        }
+        if (token.text === "{") {
+          state.atHeaderState = false;
+          return `punctuation-${getContext(state)}-start`;
+        }
+        if (token.text === ":") {
+          return `punctuation-${getContext(state)}`;
+        }
+        if (["and", "not", "or", "only"].includes(token.text.toLowerCase())) {
+          return "operator";
+        }
+        const numeric = parseNumeric(token);
+        if (numeric) {
+          return numeric.map( (type) => `${type}-${getContext(state)}`);
+        }
+      }
+    }
+
+    // exit css rule selector state, enter rule state
+    if (getContext(state) === "selector" && token.text === "{") {
+      state.contextStack[state.contextStack.length - 1] = "rule";
+      state.ruleContext = "left"
+      return "punctuation-rule-start";
+    }
+    // enter css rule selector state
+    if (
+      getContext(state) !== "rule" &&
+      getContext(state) !== "selector" &&
+      (!token.prev || ["{", "}"].includes(token?.prev?.text))
+    ) {
+      state.contextStack.push("selector");
+      return "value-selector";
+    }
+    // css rule selector state
+    if (getContext(state) === "selector") {
+      return "value-selector";
+    }
+    // exit css rule state
+    if (getContext(state) === "rule" && token.text === "}") {
+      state.contextStack.pop();
+      state.ruleContext = "none";
+      return "punctuation-rule-end";
+    }
+
+    // properties and switching to values
+    if (getContext(state) === "rule" && state.ruleContext === "left") {
+      if (token.text === ":") {
+        state.ruleContext = "right";
+        return "punctuation";
+      }
+      return "property";
+    }
+    // Switching from values back to properties
+    if (
+      getContext(state) === "rule" &&
+      state.ruleContext === "right" &&
+      token.text === ";"
+    ) {
+      state.ruleContext = "left";
+      return "punctuation";
+    }
+
+    // Function values like var(), calc() etc.
+    if (token.text.match(/[a-z-]/) && token?.next?.text === "(") {
+      return "keyword-function";
+    }
+
+    // Number values
+    const numeric = parseNumeric(token);
+    if (numeric) {
+      return numeric;
+    }
+
+    // Random punctuation
+    if (["{", "}", ":", ",", ";", ")", "("].includes(token.text)) {
+      return "punctuation";
     }
 
     // no special token
@@ -64,6 +261,34 @@ export const languageDefinition = (): ((token: LanguageToken) => string) => {
 };
 
 export const gluePredicate = (token: TypedLanguageToken): boolean => {
+  // Join @ sign and rule name
+  if (token.type.startsWith("keyword-at") && token.type === token?.prev?.type) {
+    return true;
+  }
+  // Join colons, dots and hashes with selector token directly afterwards
+  if (
+    token.type === "value-selector" &&
+    token?.prev?.type === token.type &&
+    isAdjacent(token, token.prev) &&
+    [":", ".", "#"].includes(token.prev.text)
+  ) {
+    return true;
+  }
+  // Join property names
+  if (token.type === "property" && token.prev?.type === "property") {
+    return true;
+  }
+  // Join floating point numbers
+  if (
+    (token.text === "." && token?.prev?.type === "number") ||
+    (token.type === "number" && token?.prev?.text?.endsWith("."))
+  ) {
+    return true;
+  }
+  // The tokenizer separates numbers and % signs, so we repair this here
+  if (token.text === "%" && token?.prev?.type === "number") {
+    return true;
+  }
   // Join comment text and comment signifier, but not to each other
   if (token.type === "comment" && token?.prev?.type === "comment") {
     if (token.text === "*" && token?.prev?.text === "/") {
---
+++ test/languages/css.test.ts
@@ -0,0 +1,269 @@
+import * as language from "../../src/languages/css";
+import { type } from "../helpers";
+const css = type(language);
+
+describe("Basic CSS", () => {
+  test("Simple rule", () => {
+    const tokens = css(`div { color: red; font-family: sans-serif; }`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "token",
+      "punctuation",
+      "property",
+      "punctuation",
+      "token",
+      "token",
+      "token",
+      "punctuation",
+      "punctuation-rule-end",
+    ]);
+  });
+  test("Simple rule with non-trivial selector", () => {
+    const tokens = css(`div + .foo, :host([disabled="true"]) ~ a { color: red; }`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "string",
+      "string",
+      "string",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "token",
+      "punctuation",
+      "punctuation-rule-end",
+    ]);
+  });
+  test("Two simple rules", () => {
+    const tokens = css(`div { color: red; } #foo { font-family: sans-serif; }`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "token",
+      "punctuation",
+      "punctuation-rule-end",
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "token",
+      "token",
+      "token",
+      "punctuation",
+      "punctuation-rule-end",
+    ]);
+  });
+  test("Custom properties", () => {
+    const tokens = css(`:root { --foo: red } .foo { color: var(--foo) }`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "token",
+      "punctuation-rule-end",
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "keyword-function",
+      "punctuation",
+      "token",
+      "token",
+      "token",
+      "punctuation",
+      "punctuation-rule-end",
+    ]);
+  });
+});
+
+describe("At-rules", () => {
+  test("Simple media query", () => {
+    const tokens = css(`@media print {}`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-media",
+      "keyword-media-type",
+      "punctuation-at-media-start",
+      "punctuation-at-media-end",
+    ]);
+  });
+
+  test("Simple media query with media features", () => {
+    const tokens = css(`@media (min-width: 800px) {}`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-media",
+      "punctuation-at-media-argument",
+      "token",
+      "token",
+      "token",
+      "punctuation-at-media",
+      "number-at-media",
+      "punctuation-at-media-argument",
+      "punctuation-at-media-start",
+      "punctuation-at-media-end",
+    ]);
+  });
+
+  test("Simple media query with media type and features", () => {
+    const tokens = css(`@media screen and (min-width: 800px) {}`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-media",
+      "keyword-media-type",
+      "operator",
+      "punctuation-at-media-argument",
+      "token",
+      "token",
+      "token",
+      "punctuation-at-media",
+      "number-at-media",
+      "punctuation-at-media-argument",
+      "punctuation-at-media-start",
+      "punctuation-at-media-end",
+    ]);
+  });
+
+  test("Nested media queries", () => {
+    const tokens = css(`@media screen and (min-width: 800px) { @media print {} }`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-media",
+      "keyword-media-type",
+      "operator",
+      "punctuation-at-media-argument",
+      "token",
+      "token",
+      "token",
+      "punctuation-at-media",
+      "number-at-media",
+      "punctuation-at-media-argument",
+      "punctuation-at-media-start",
+      "keyword-at-media",
+      "keyword-media-type",
+      "punctuation-at-media-start",
+      "punctuation-at-media-end",
+      "punctuation-at-media-end",
+    ]);
+  });
+
+  test("Simple @supports query", () => {
+    const tokens = css(`@supports (display: flex) {}`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-supports",
+      "punctuation-at-supports-argument",
+      "token",
+      "punctuation-at-supports",
+      "token",
+      "punctuation-at-supports-argument",
+      "punctuation-at-supports-start",
+      "punctuation-at-supports-end",
+    ]);
+  });
+
+  test("@import with bare string", () => {
+    const tokens = css(`@import "foo.css"`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-import",
+      "string",
+      "string",
+      "string",
+      "string",
+      "string",
+    ]);
+  });
+
+  test("@import with quoted url", () => {
+    const tokens = css(`@import url("foo.css")`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-import",
+      "keyword-url",
+      "punctuation-url",
+      "string-url",
+      "string-url",
+      "string-url",
+      "string-url",
+      "string-url",
+      "punctuation-url",
+    ]);
+  });
+
+  test("@import with unquoted url", () => {
+    const tokens = css(`@import url(foo.css)`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-import",
+      "keyword-url",
+      "punctuation-url",
+      "string-url",
+      "string-url",
+      "string-url",
+      "punctuation-url",
+    ]);
+  });
+
+  test("@keyframes", () => {
+    const tokens = css(`@keyframes foo {
+  from { opacity: 0; }
+  30% { opacity: 0.5; }
+  68%, 72% { opacity: 0.9; }
+}`);
+    const types = tokens.map((token) => token.type);
+    expect(types).toEqual([
+      "keyword-at-keyframes",
+      "token",
+      "punctuation-at-keyframes-start",
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "number",
+      "punctuation",
+      "punctuation-rule-end",
+      "value-selector",
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "number",
+      "punctuation",
+      "punctuation-rule-end",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "value-selector",
+      "punctuation-rule-start",
+      "property",
+      "punctuation",
+      "number",
+      "punctuation",
+      "punctuation-rule-end",
+      "punctuation-at-keyframes-end",
+    ]);
+  });
+});
