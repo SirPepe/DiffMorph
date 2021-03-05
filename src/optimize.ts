f085ff0f943fab3efd11e7d2f361b882921baca4
Peter Kröner <peter@peterkroener.de>
2021-03-05 13:21:41 +0100

    start work on the optimizer

--- src/optimize.ts
+++ src/optimize.ts
@@ -11,22 +11,22 @@ export function optimize<T extends TokenLike>(
 }
 
 function optimizeFrame<T extends TokenLike>(diff: DiffOp<T>[]): DiffOp<T>[] {
-  const byHash: Record<string, [MOV<T>[], ADD<T>[], DEL<T>[]]> = {};
+  const byHash: Record<string, [Set<MOV<T>>, Set<ADD<T>>, Set<DEL<T>>]> = {};
   for (const op of diff) {
     if (!byHash[op.item.hash]) {
-      byHash[op.item.hash] = [[], [], []];
+      byHash[op.item.hash] = [new Set(), new Set(), new Set()];
     }
     if (op.type == "MOV") {
-      byHash[op.item.hash][0].push(op);
+      byHash[op.item.hash][0].add(op);
     } else if (op.type == "ADD") {
-      byHash[op.item.hash][1].push(op);
+      byHash[op.item.hash][1].add(op);
     } else if (op.type === "DEL") {
-      byHash[op.item.hash][2].push(op);
+      byHash[op.item.hash][2].add(op);
     }
   }
   const result: DiffOp<T>[] = [];
   for (const [MOV, ADD, DEL] of Object.values(byHash)) {
-    if (ADD.length === 0 || DEL.length === 0) {
+    if (ADD.size === 0 || DEL.size === 0) {
       result.push(...MOV, ...ADD, ...DEL);
     } else {
       result.push(...MOV, ...resolveOptimizations(ADD, DEL));
@@ -36,8 +36,31 @@ function optimizeFrame<T extends TokenLike>(diff: DiffOp<T>[]): DiffOp<T>[] {
 }
 
 function resolveOptimizations<T extends TokenLike>(
-  additions: ADD<T>[],
-  deletions: DEL<T>[]
+  additions: Set<ADD<T>>,
+  deletions: Set<DEL<T>>
 ): DiffOp<T>[] {
-  return [...additions, ...deletions];
+  const movements: MOV<T>[] = [];
+  for (const deletion of deletions) {
+    const alternative = pickAlternative(deletion, additions);
+    if (alternative) {
+      movements.push({ type: "MOV", item: alternative.item, ref: deletion.item });
+      additions.delete(alternative);
+      deletions.delete(deletion);
+      continue;
+    }
+    if (additions.size === 0) {
+      break;
+    }
+  }
+  return [...additions, ...deletions, ...movements];
+}
+
+function pickAlternative<T extends TokenLike>(
+  deletion: DEL<T>,
+  candidates: Set<ADD<T>>
+): ADD<T> | null {
+  if (candidates.size === 1) {
+    return Array.from(candidates)[0];
+  }
+  return null;
 }
---
+++ test/optimize.test.ts
@@ -0,0 +1,23 @@
+import { diffAll } from "../src/diff";
+import * as language from "../src/languages/json";
+import { optimize } from "../src/optimize";
+import { type } from "./helpers";
+const json = type(language);
+
+describe("Optimizer", () => {
+  test("It turns a single addition/deletion into a movement", () => {
+    const res = optimize(diffAll([json("{}"), json("{ }")]));
+    expect(res.length).toBe(2);
+    expect(res[0].map( (op) => op.type)).toEqual(["ADD", "ADD"]);
+    expect(res[1].length).toBe(1);
+    expect(res[1][0].item).toMatchObject({ x: 2, y: 0 });
+    expect((res[1][0] as any).ref).toMatchObject({ x: 1, y: 0 });
+  });
+
+  test.skip("It keeps the last token on the end of the line", () => {
+    const res = optimize(diffAll([json("[]"), json("[[]]")]));
+    expect(res.length).toBe(2);
+    expect(res[0].map( (op) => op.type)).toEqual(["ADD", "ADD"]);
+    console.log(res[1]);
+  });
+});
