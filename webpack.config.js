90d04ac47895702e86581ba11439082a070fcb03
Peter Kröner <peter@peterkroener.de>
2021-03-12 13:09:37 +0100

    tweak build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const path = require("path");
-
-module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  entry: "./src/element/diff-morph.ts",
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "diff-morph.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: "babel-loader" },
-          { loader: "ts-loader" },
-        ]
-      }
-    ]
-  }
+module.exports = (env) => {
+  const mode = env.mode || "production";
+  const isProd = env.mode === "production";
+  return {
+    mode,
+    devtool: (isProd) ? "inline-source-map" : false,
+    entry: "./src/element/diff-morph.ts",
+    resolve: {
+      extensions: [".ts", ".js"]
+    },
+    output: {
+      path: require("path").resolve(__dirname, "dist"),
+      filename: "diff-morph.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: "babel-loader" },
+            { loader: "ts-loader" },
+          ]
+        }
+      ]
+    }
+  };
 };
