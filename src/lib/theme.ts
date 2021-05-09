d7227b858bb692d73f71b6581d3590da0d21eb5d
Peter Kröner <peter@peterkroener.de>
2021-05-09 12:54:49 +0200

    use hexadecimal colors in default theme for compatibility with color input elements

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -20,16 +20,18 @@ export type ThemeProperties = {
 
 export type Theme = Record<string, ThemeProperties>;
 
+// These should be six-character hexadecimal values for compatibility with
+// color input elements
 export const DEFAULT_COLORS = {
-  foreground: "#000",
-  background: "#FFF",
-  string: "hsl(340, 95%, 38%)",
-  number: "hsl(170, 100%, 25%)",
-  comment: "hsl(0, 0%, 50%)",
-  global: "hsl(215, 100%, 40%)",
-  type: "hsl(207, 75%, 25%)",
-  tag: "hsl(185, 100%, 17.5%)",
-  value: "hsl(135, 100%, 28%)",
-  literal: "hsl(280, 70%, 50%)",
-  punctuation: "hsl(0, 0%, 25%)",
+  foreground: "#000000",
+  background: "#ffffff",
+  string: "#bd0542",
+  number: "#00806a",
+  comment: "#808080",
+  global: "#0055cc",
+  type: "#104570",
+  tag: "#004f57",
+  value: "#008f24",
+  literal: "#9d26d9",
+  punctuation: "#404040",
 };
