7fd50ba2d9dbd41b0af7650e359c6667742d00d7
Peter Kröner <peter@peterkroener.de>
2021-06-15 11:35:57 +0200

    move TokenPool class into its own file

--- src/render/render.ts
+++ src/render/render.ts
@@ -1,6 +1,4 @@
-// Assigns concrete render objects based on lifecycles. The outcome of this
-// module could be probably be improved by prioritizing MOV ops over all other
-// operations across all lifecycles.
+// Turn lifecycles into useable object graphs for rendering.
 
 import {
   DecorationPosition,
@@ -12,121 +10,10 @@ import {
   RenderRoot,
   RenderText,
   TextPosition,
-  Token,
-  ADD,
-  BAD,
-  BDE,
-  DEL,
-  MOV,
 } from "../types";
-import { BoxLifecycle, Lifecycle } from "../render/lifecycle";
-import { assertIs, createIdGenerator, toString } from "../util";
-
-type InputToken = Token & { hash: number };
-type OutputToken = { id: string };
-
-function toRenderPosition<Input extends Token>(
-  { x, y, width, height }: Input,
-  id: string,
-  isVisible: boolean
-): TextPosition {
-  return { id, x, y, width, height, alpha: Number(isVisible) };
-}
-
-class TokenPool<Input extends InputToken, Output extends OutputToken> {
-  private nextId = createIdGenerator();
-
-  // hash -> list of currently not used output token
-  private available = new Map<number, Output[]>();
-
-  // holder -> [output, last position]
-  private inUse = new Map<Lifecycle<Input>, [Output, TextPosition]>();
-
-  // Customize how an input token turns into an output token
-  constructor(private toOutput: (item: Input, newId: string) => Output) {}
-
-  private require(
-    holder: Lifecycle<Input>,
-    { kind, item }: ADD<Input> | BAD<Input> | MOV<Input> | BDE<Input>
-  ): [Output, TextPosition] {
-    const isVisible = kind === "ADD" || kind === "MOV";
-    const available = this.available.get(item.hash);
-    if (available && available.length > 0) {
-      const output = available.shift() as Output;
-      const position = toRenderPosition(item, output.id, isVisible);
-      this.inUse.set(holder, [output, position]);
-      return [output, position];
-    } else {
-      if (!available) {
-        // New list remains empty as the output token goes into use right away
-        this.available.set(item.hash, []);
-      }
-      const nextId = this.nextId(null, toString(item.hash));
-      const output = this.toOutput(item, nextId);
-      const position = toRenderPosition(item, output.id, isVisible);
-      this.inUse.set(holder, [output, position]);
-      return [output, position];
-    }
-  }
-
-  // Get the render token that was last used for a specific lifecycle or create
-  // a new one depending on the input operation. If there's no input operation,
-  // attempt to continue with the lifecycle without changes (as that's probably
-  // a case of a unchanged token between frames)
-  public use(
-    holder: Lifecycle<Input>,
-    operation?: ADD<Input> | BAD<Input> | MOV<Input> | BDE<Input>
-  ): [Output, TextPosition] | undefined {
-    const previousOutput = this.inUse.get(holder);
-    // In case there is no current operation, see if the holder holds onto
-    // something and re-use the existing information
-    if (!operation) {
-      return previousOutput;
-    }
-    // If there is a current operation, but the holder holds onto nothing,
-    // create a new token
-    if (!previousOutput) {
-      return this.require(holder, operation);
-    }
-    // Otherwise perform an update to tha last position
-    const isVisible = operation.kind === "MOV" || operation.kind === "ADD";
-    const newPosition = toRenderPosition(
-      operation.item,
-      previousOutput[0].id,
-      isVisible
-    );
-    previousOutput[1] = newPosition; // update the last position
-    return [previousOutput[0], newPosition];
-  }
-
-  // For DEL: free used token
-  public free(holder: Lifecycle<Input>, operation: DEL<Input>): undefined {
-    const freed = this.inUse.get(holder);
-    if (!freed) {
-      // This can happen for DEL ops in the first frame, which in turn may be
-      // inserted there by lifecycle extensions.
-      return;
-    }
-    const list = this.available.get(operation.item.hash);
-    if (!list) {
-      throw new Error("List for hash not in reserved map!");
-    }
-    list.push(freed[0]);
-    this.inUse.delete(holder);
-  }
-}
-
-function renderToken<Input extends InputToken, Output extends OutputToken>(
-  lifecycle: Lifecycle<Input>,
-  frame: number,
-  pool: TokenPool<Input, Output>
-): [Output, TextPosition] | undefined {
-  const operation = lifecycle.get(frame);
-  if (operation && operation.kind === "DEL") {
-    return pool.free(lifecycle, operation);
-  }
-  return pool.use(lifecycle, operation);
-}
+import { BoxLifecycle } from "../render/lifecycle";
+import { assertIs } from "../util";
+import { renderToken, TokenPool, toRenderPosition } from "./TokenPool";
 
 function toRenderText({ type, text }: DiffTokens, id: string): RenderText {
   return { type, text, id };
